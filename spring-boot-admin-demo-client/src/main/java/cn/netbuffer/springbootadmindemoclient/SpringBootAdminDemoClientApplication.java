cae60652e252e07b8c6b9ba3661d74c8cf40c313
netbuffer <javawiki@163.com>
2021-09-15 11:36:41 +0800

    enable httptrace

--- spring-boot-admin-demo-client/src/main/java/cn/netbuffer/springbootadmindemoclient/SpringBootAdminDemoClientApplication.java
+++ spring-boot-admin-demo-client/src/main/java/cn/netbuffer/springbootadmindemoclient/SpringBootAdminDemoClientApplication.java
@@ -1,10 +1,18 @@
 package cn.netbuffer.springbootadmindemoclient;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootAdminDemoClientApplication {
+
+    @Bean
+    public HttpTraceRepository buildHttpTraceRepository(){
+        return new InMemoryHttpTraceRepository();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAdminDemoClientApplication.class, args);
